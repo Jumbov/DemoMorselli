2b89f6fde099129bdf672b7b6bc7f1af8d981123
Luca <Luca@DESKTOP-T600TTG>
2020-05-09 17:23:14 +0200

    primo commit

---
+++ src/main/java/com/example/demo/controllers/RestController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.luca.logic.BibliotecaManager;
+import org.luca.pojos.Biblioteca;
+
+public class RestController {
+	
+	public void metodo1() {
+		
+
+		List<String> libri = new ArrayList<String>();
+		libri.add("Pnl per il carisma");
+		libri.add("Amore libero");
+		libri.add("Cinciallegre");
+		libri.add("Natura e civiltà");
+		
+		
+		Biblioteca b = new Biblioteca();
+		BibliotecaManager bm = new BibliotecaManager();
+		bm.ordinaAscendente(libri);
+		
+		System.out.println(libri);
+		
+	}
+
+}
